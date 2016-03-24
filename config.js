abcad61d8885123051306832f0c4b8e461718776
Kai Walter <kai.walter@outlook.de>
2016-03-24 19:38:38 +0100

    inserted placeholders again

--- config.js
+++ config.js
@@ -3,12 +3,12 @@
  exports.creds = {
  	returnURL: 'http://localhost:3000',
  	identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
- 	clientID: 'ac55a187-c663-4236-970c-65807dc4413d',
+ 	clientID: '<your app id>',
  	skipUserProfile: true, // for AzureAD should be set to true.
  	responseType: 'id_token', // for login only flows use id_token. For accessing resources use `id_token code`
  	responseMode: 'form_post', // For login only flows we should have token passed back to us in a POST
  	// scope: ['email', 'profile'], // additional scopes you may wish to pass
- 	tenantName: 'zeissb2cdev.onmicrosoft.com', 
+ 	tenantName: '<your tentant>.onmicrosoft.com', 
  	validateIssuer: true, // if you have validation on, you cannot have users from multiple tenants sign in
  	passReqToCallback: false,
  	loggingLevel: 'info' // valid are 'info', 'warn', 'error'. Error always goes to stderr in Unix.     
